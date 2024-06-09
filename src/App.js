173fcb916b4a3fe632db8a5b70d250734c1a1976
KALLEL Mahmoud <mahmoud.kallel@sagemcom.com>
2024-06-09 12:37:33 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import init, { create_device, Device } from './pkg/refresh_device';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [device, setDevice] = useState(null);
+    const [firmwareVersion, setFirmwareVersion] = useState('');
+    const [serialNumber, setSerialNumber] = useState('');
+
+    useEffect(() => {
+        const run = async () => {
+            await init();
+            const deviceInstance = create_device('12345', '1.0.0');
+            setDevice(deviceInstance);
+            setFirmwareVersion(deviceInstance.get_firmware_version());
+            setSerialNumber(deviceInstance.get_serial_number());
+        };
+        run();
+    }, []);
+
+    const updateFirmware = () => {
+        if (device) {
+            device.update_firmware('2.0.0');
+            setFirmwareVersion(device.get_firmware_version());
+        }
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Serial Number: {serialNumber}</p>
+                <p>Firmware Version: {firmwareVersion}</p>
+                <button onClick={updateFirmware}>Update Firmware</button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
